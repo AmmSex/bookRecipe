60619415531cf7212a0865657c0edc4bc6586a02
AmmSex <prist_as@vk.com>
2022-04-15 16:50:12 +0300

    fix ColorList

--- src/ColorList/index.jsx
+++ src/ColorList/index.jsx
@@ -1,19 +1,30 @@
 import { render } from "@testing-library/react";
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import Color from "../Color";
-import {ColorContext} from '../index'
- 
-const ColorList = ({onRemoveColor = f => f, onRateColor = f => f}) => {
-  const {colors} = useContext(ColorContext)
-  if (!colors.length) return <div>No Color Listed.</div>;
-  return(
-    <>
-      {colors.map((el) => (
-        <Color key={el.id} {...el} onRemove={onRemoveColor} onRate={onRateColor}/>
-      ))}
-    </>
+import { ColorContext } from "../index";
+
+const ColorList = ({ onRemoveColor = (f) => f, onRateColor = (f) => f }) => {
+  //const {colors} = useContext(ColorContext)
+
+  return (
+    <ColorContext.Consumer>
+      {context => {
+        if (!context.colors.length) return <div>No Color Listed.</div>;
+        return (
+          <>
+            {context.colors.map((el) => (
+              <Color
+                key={el.id}
+                {...el}
+                onRemove={onRemoveColor}
+                onRate={onRateColor}
+              />
+            ))}
+          </>
+        );
+      }}
+    </ColorContext.Consumer>
   );
 };
-
 
 export default ColorList;
